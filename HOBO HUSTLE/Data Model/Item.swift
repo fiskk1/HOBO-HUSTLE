76482806c908bef4f9719dce20661468290c4580
Kyle Fisk <kfisk1@melindas-macbook-air.local>
2018-03-07 08:42:06 -0500

    Convert to MVC design pattern.

---
+++ HOBO HUSTLE/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  HOBO HUSTLE
+//
+//  Created by Kyle Fisk on 3/7/18.
+//  Copyright © 2018 BOHO HOBO. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
