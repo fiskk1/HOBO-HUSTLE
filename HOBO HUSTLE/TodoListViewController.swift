85fb935da0d84e30b289629171f5db88fa452844
Kyle Fisk <kfisk1@melindas-macbook-air.local>
2018-03-06 21:34:56 -0500

    Data persisted using UserDefaults method.

--- HOBO HUSTLE/TodoListViewController.swift
+++ HOBO HUSTLE/TodoListViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Meditate", "Yoga", "Skateboard"]
+    var itemArray = ["Meditate", "Yoga", "Skateboard"]
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        if let items = defaults.array(forKey: "HoboListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
     
     //MARK - Tableview Datasource Methods
@@ -50,5 +56,42 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    //MARK - Add New Items
+
+ 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item button on our UIAlert
+            
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "HoboListArray")
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
+
+
+
+
+
+
+
+
 
